a5ef79e6eb11d4a0c52aa74dd07a535927732704
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-08-29 13:34:28 +0900

    エルコインページの更新

--- resources/views/LCoin/index.blade.php
+++ resources/views/LCoin/index.blade.php
@@ -6,10 +6,8 @@
 @endsection
       
 @section('content')
-@if ($msg !='')
-<p> {{$msg}}</p>
-@endif
-<div class="bg-white py-6 sm:py-8 lg:py-12">
+
+<div class="bg-white py-2 sm:py-8 lg:py-12">
 
   <div class="max-w-screen-lg px-4 md:px-8 mx-auto">
     <!-- 生徒単位のFOR分 1人の保護者に対して複数の生徒がいることを想定-->
@@ -17,10 +15,11 @@
 
     <div class="grid md:grid-cols-2 gap-8 mb-8">
       <!-- images - start -->
-      <div class="space-y-4">
+      <div class="space-y-1">
         <!-- <div class="bg-gray-100 rounded-lg overflow-hidden relative"> -->
-        <div class="bg-gray-100 rounded-lg overflow-hidden">
-          <img src="https://images.unsplash.com/flagged/photo-1571366992942-be878c7b10c0?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Himanshu Dewangan" class="w-full h-full object-cover object-center" />
+        <div class="bg-gray-100 rounded-lg overflow-hidden xl:w-2/3 h-60 lg:h-auto bg-gray-100 overflow-hidden shadow-lg">
+          <!-- <img src="https://images.unsplash.com/flagged/photo-1571366992942-be878c7b10c0?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Himanshu Dewangan" class="w-full h-full object-cover object-center" /> -->
+          <img src="/images/lcoin_mainimage.gif" loading="lazy" alt="エルコイン" class="w-full h-full object-cover object-center" />
         </div>
       </div>
       <!-- images - end -->
@@ -29,7 +28,7 @@
       <div class="md:py-8">
         <!-- name - start -->
         <div class="mb-2 md:mb-3">
-          <span class="inline-block text-gray-500 mb-0.5">エルコイン残高</span>
+          <span class="inline-block text-gray-600 mb-0.5">エルコイン残高</span>
           <h2 class="text-gray-800 text-2xl lg:text-3xl font-bold">{{$itemset[$i][0]->StudentName}}さん</h2>
         </div>
         <!-- name - end -->
@@ -43,17 +42,18 @@
         <!-- price - end -->
 
         <!-- description - start -->
-        <div class="mt-10 md:mt-16 lg:mt-20">
-          <span class="text-gray-800 text-lg font-semibold mb-3">コイン明細</span>
+        <div class="mt-6 md:mt-8 lg:mt-20">
+          <span class="text-gray-600 text-lg font-semibold mb-3">コイン明細</span>
           <table class="table-fixed text-base">
             <tr>
-                <th class="w-2/9 bg-gray-300 text-slate-200 px-2 py-2">発生日</th>
+                <th class="w-2/9 bg-gray-300 text-slate-50 px-2 py-2">発生日</th>
                 <th class="w-2/9 bg-gray-300 text-slate-50 px-2 py-2">コイン数</th>
                 <th class="w-2/9 bg-gray-300 text-slate-50 px-2 py-2">事由</th>
                 <th class="w-1/3 bg-gray-300 text-slate-50 px-2 py-2">事由補足</th>
             </tr>
             @foreach($itemset[$i] as $item)
-            <tr class="hover:bg-gray-400">
+            <!-- <tr class="hover:bg-gray-400"> -->
+            <tr class="">
                 <td class="px-2 py-2">{{$item->HasseiDate}}</td>
                 <td class="px-2 py-2">{{$item->amount}}</td>
                 <td class="px-2 py-2">{{$item->Ziyuu}}</td>
