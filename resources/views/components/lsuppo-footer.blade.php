b009877a2b8a04c4119f4153230cd51967304577
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-09-06 03:45:11 +0900

    フッターにリンク追加

--- resources/views/components/lsuppo-footer.blade.php
+++ resources/views/components/lsuppo-footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="w-full item-center">
-    <p class="mx-8 text-center xl:text-left text-gray-500">© プログラミングスクールL</p>
+    <p class="mx-8 text-center xl:text-left text-gray-500">© <a href="https://manabiail-steam.com" class="hover:text-indigo-500 hover:underline-offset-2">プログラミングスクールL</a></p>
     </div>
 </footer>
