6b61ef9fbe8db1413528a091662ee0dd909a9b81
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-12-23 08:13:20 +0900

    リンク修正

--- resources/views/components/lsuppo-header.blade.php
+++ resources/views/components/lsuppo-header.blade.php
@@ -42,7 +42,7 @@
                         </div>
                     </div>
                 </li>
-                <li class="border-b-2 md:border-none md:h-4"><a href="/photo/index/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">フォトギャラリー</a></li>
+                <li class="border-b-2 md:border-none md:h-4"><a href="/photos/index/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">フォトギャラリー</a></li>
                 <li class="border-b-2 md:border-none md:h-4"><a href="/lc/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">エルコイン</a></li>
                 <!-- 生徒紐づけが完了していない保護者 -->
                 @elsecan('hogosha-nobind')
@@ -51,7 +51,7 @@
                 @elsecan('supporter-binded')
                 <li class="border-b-2 md:border-none md:h-4"><a href="/supporter-page/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">ホーム</a></li>
                 <li class="border-b-2 md:border-none md:h-4"><a href="/fb/index_sp" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">フィードバック</a></li>
-                <li class="border-b-2 md:border-none md:h-4"><a href="/photo/index/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">フォトギャラリー</a></li>
+                <li class="border-b-2 md:border-none md:h-4"><a href="/photos/index/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">フォトギャラリー</a></li>
                 <!-- サポーターマスタとの紐づけが完了していないサポーター -->
                 @elsecan('supporter-nobind')
                 <li class="border-b-2 md:border-none md:h-4"><a href="/supporter-page/" class="block px-8 py-2 my-2 hover:bg-gray-600  rounded">ホーム</a></li>
