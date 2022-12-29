c953b6efe509b3cba342adec0fcae21492cc53d9
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-12-30 07:54:18 +0900

    toggleのコンポーネント化

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -31,11 +31,13 @@
                     <p class="text-sm mt-2 text-gray-700 w-2/3 mx-auto">LINEでログインできるようにするにはまずこちらを押してLINE紐づけをONにしてください</p>
                 </div>
                 <div class="w-fit mx-auto">
-                    <label class="inline-flex relative items-center cursor-pointer">
+                    {{-- Component化 --}}
+                    {{-- <label class="inline-flex relative items-center cursor-pointer">
                         <input type="checkbox" value="" class="sr-only peer" disabled {{$bindChecked}}>
                         <div class="w-14 h-7 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-teal-600"></div>
                         <span class="ml-3 text-sm font-medium text-gray-400 dark:text-gray-500">LINE 紐づけ</span>
-                    </label>
+                    </label> --}}
+                    <x-lsuppo-toggle :bindChecked="$bindChecked" :description="'LINE 紐づけ'" />
                 </div>
             </div>
         </div>
