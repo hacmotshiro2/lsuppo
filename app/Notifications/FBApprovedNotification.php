aabbbbfa029caa2dba2430343c8254d62c7b8645
mots-and-code <hacmotshiro2@yahoo.co.jp>
2023-01-02 22:52:14 +0900

    LINE通知 バグFix

--- app/Notifications/FBApprovedNotification.php
+++ app/Notifications/FBApprovedNotification.php
@@ -9,6 +9,23 @@ use Illuminate\Notifications\Notification;
 
 use App\Notifications\LSuppoNotification;
 
+use Illuminate\Support\Facades\Log;
+
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+
+
 class FBApprovedNotification extends LSuppoNotification
 // class FBApprovedNotification extends Notification
 {
