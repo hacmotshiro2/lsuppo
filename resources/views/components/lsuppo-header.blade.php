207b8e519caa9a26ed8aa82a8298fb87f07b906e
hash-home-hacmo <hacmotshiro2@gmail.com>
2023-07-31 07:14:25 +0900

    v-on:clickの書き方変更

--- resources/views/components/lsuppo-header.blade.php
+++ resources/views/components/lsuppo-header.blade.php
@@ -8,7 +8,7 @@
         </div>
         <!-- ハンバーガーメニュー -->
         <div class="mx-4">
-            <button class="md:hidden focus:outline-none" @click="isOpen = !isOpen">
+            <button class="md:hidden focus:outline-none" v-on:click="isOpen = !isOpen">
                 <!-- 768px以上でハンバーガーメニュー非表示に hiddenにするだけ -->
                 <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                     <path v-show="!isOpen" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
